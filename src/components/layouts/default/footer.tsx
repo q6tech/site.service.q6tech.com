4a8daea10576d2af50edc99735e563f6a453c535
Mark Lopez <m@silvenga.com>
2024-10-05 09:25:42 -0500

    Improved visual contrast in footer, added copyright.

--- src/components/layouts/default/footer.tsx
+++ src/components/layouts/default/footer.tsx
@@ -1,6 +1,8 @@
+import { useMemo } from "react";
 import { Link } from "react-router-dom";
 
 export function Footer() {
+    const year = useMemo(() => new Date().getFullYear(), []);
     return (
         <footer className="h-64 text-gray-300 bg-gray-950">
             <div className="w-full h-6 bg-white rounded-b-xl relative z-20">
@@ -8,15 +10,15 @@ export function Footer() {
             </div>
             <div className="absolute bottom-0 right-0 left-0 h-64 z-0 pt-[calc(100lvh-100svh)]">
                 <div className="container mx-auto p-6 pt-12 flex h-full">
-                    <div>
-                        <p className="font-semibold">Q6 Technologies, LLC</p>
-                        <p className="">Made with Love in Texas, USA.</p>
+                    <div className="grow">
+                        <p className="font-semibold text-white">Q6 Technologies, LLC</p>
+                        <p className="mt-3">Made with Love in Texas, USA.</p>
+                        <p className="">Copyright © {year}</p>
                     </div>
-
-                    <div className="flex flex-col ms-auto">
+                    <div className="flex flex-col ps-3">
                         <div className="font-semibold border-b mb-3 pb-1 border-gray-400">Links</div>
-                        <Link to="/meet-the-team" className="mb-1 link link-hover">Meet the Team</Link>
-                        <Link to="/contact-us" className="mb-1 link link-hover">Contact Us</Link>
+                        <Link to="/meet-the-team" className="mb-1 link md:link-hover text-white">Meet the Team</Link>
+                        <Link to="/contact-us" className="mb-1 link md:link-hover text-white">Contact Us</Link>
                     </div>
                 </div>
             </div>
