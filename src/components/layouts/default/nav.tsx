b0c2e90f401ab7ea13d44ccec1f30324398690dc
Mark Lopez <m@silvenga.com>
2024-10-04 20:19:16 -0500

    Improved padding under mobile.

--- src/components/layouts/default/nav.tsx
+++ src/components/layouts/default/nav.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { useEffect, useState } from "react";
 import { FaPaperPlane, FaPeoplePulling } from "react-icons/fa6";
 import { TbMenu2 } from "react-icons/tb";
@@ -13,7 +14,7 @@ export function Nav() {
 
     return (
         <div className="fixed w-full bg-white/60 backdrop-blur-xl select-none z-20 border-b">
-            <nav className="container mx-auto flex mt-6 mb-5 px-6 items-center" role="banner">
+            <nav className="container mx-auto flex my-3 md:mt-6 md:mb-5 md:px-6 items-center" role="banner">
                 <div className="flex items-center">
                     <Link to="/" className="brand-mark h-8 me-2 mt-1" aria-label="Q6 Technologies Logo"></Link>
                 </div>
@@ -32,7 +33,7 @@ export function Nav() {
                                 <div className="flex items-center mt-6">
                                     <Link to="/" className="brand-mark h-8 me-2 mt-1" aria-label="Q6 Technologies Logo"></Link>
                                 </div>
-                                <ul className="list-none -ms-9 mt-6">
+                                <ul className="list-none mt-6">
                                     <NavItems />
                                 </ul>
                                 <div className="mt-auto p-6 pt-24 opacity-70 text-sm">
@@ -51,13 +52,25 @@ export function Nav() {
 function NavItems() {
     return (
         <>
-            <li className="ms-9 flex items-center">
-                <FaPeoplePulling className="me-2" />
-                <NavLink to="/meet-the-team" className="link link-hover">Meet the Team</NavLink>
+            <li>
+                <NavLink to="/meet-the-team" className="md:ms-9 mb-3 md:mb-0 flex items-center justify-center border md:border-0 rounded-lg p-3 md:p-0">
+                    {({ isActive }) => (
+                        <>
+                            <FaPeoplePulling className="me-2" />
+                            <span className={clsx("link link-hover", isActive && "underline")}>Meet the Team</span>
+                        </>
+                    )}
+                </NavLink>
             </li>
-            <li className="ms-9 flex items-center">
-                <FaPaperPlane className="me-2" />
-                <NavLink to="/contact-us" className="link link-hover">Contact Us</NavLink>
+            <li>
+                <NavLink to="/contact-us" className="md:ms-9 mb-3 md:mb-0 flex items-center justify-center border md:border-0 rounded-lg p-3 md:p-0">
+                    {({ isActive }) => (
+                        <>
+                            <FaPaperPlane className="me-2" />
+                            <span className={clsx("link link-hover", isActive && "underline")}>Contact Us</span>
+                        </>
+                    )}
+                </NavLink>
             </li>
         </>
     );
