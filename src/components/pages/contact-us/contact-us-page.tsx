0286dcb7ff1e4524707222b769de47c10eb63c39
Mark Lopez <m@silvenga.com>
2024-10-05 09:55:57 -0500

    Prototyped improved contact us page.

--- src/components/pages/contact-us/contact-us-page.tsx
+++ src/components/pages/contact-us/contact-us-page.tsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async";
+import { FaChevronRight } from "react-icons/fa6";
 
 export function ContactUsPage() {
     return (
@@ -13,15 +14,9 @@ export function ContactUsPage() {
                     Contact Us!
                 </h1>
                 <div className="mx-auto w-24 mt-5 h-1 bg-purple-900" />
-                <p className="mb-0">
-                    Want to setup a video call, get a quote, or just say hi?
-                </p>
-                <p className="mt-0">
-                    We're just an email away!
-                </p>
             </header>
 
-            <article className="flex flex-col items-center justify-center w-96 mx-auto px-24 pt-9 pb-12 relative">
+            {/* <article className="flex flex-col items-center justify-center w-96 mx-auto px-24 pt-9 pb-12 relative">
 
                 <a className="text-2xl px-6 py-3 rounded-2xl bg-purple-900 text-white mx-auto mt-6 transition-colors hover:bg-purple-800" href="mailto:hello@q6tech.com" role="button">Email Us</a>
 
@@ -31,6 +26,35 @@ export function ContactUsPage() {
 
                 <div className="absolute hidden md:block top-4 left-4 h-10 w-10 border-purple-900 border-t-4 border-s-4" />
                 <div className="absolute hidden md:block bottom-4 right-4 h-10 w-10 border-purple-900 border-b-4 border-e-4" />
+
+            </article> */}
+
+
+            <article className="w-full max-w-[72ch] rounded-xl mx-auto bg-purple-900 p-12 flex flex-col">
+
+                <div className="prose text-white">
+                    <span className="text-6xl font-light">
+                        Let's Talk!
+                    </span>
+
+                    <p className="mb-0">
+                        Want to setup a video call, get a quote, or just say hi?
+                    </p>
+                    <p className="mt-0">
+                        We're just an email away!
+                    </p>
+                </div>
+
+                <div className="ms-auto mt-16 relative p-12">
+                    <a className="text-2xl px-6 py-3 rounded-2xl bg-purple-800 text-white transition-colors hover:bg-purple-700 font-semibold"
+                        href="mailto:hello@q6tech.com"
+                        role="button">
+                        hello@q6tech.com
+                    </a>
+
+                    <div className="absolute hidden md:block top-4 left-4 h-10 w-10 border-white border-t-4 border-s-4" />
+                    <div className="absolute hidden md:block bottom-4 right-4 h-10 w-10 border-white border-b-4 border-e-4" />
+                </div>
 
             </article>
 
